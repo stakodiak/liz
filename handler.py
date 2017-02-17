de8c635d19b861facd4819604def4a29a6014656
Alex Stachowiak <alexander@computer.org>
2017-02-17 01:26:01 -0600

    Add more sample files to base build.

--- handler.py
+++ handler.py
@@ -1,20 +1,100 @@
 #!/usr/bin/python
 """usage: liz <command> [<args>]"""
+import json
 import os
 import getopt
 import sys
+
+
+SAMPLE_STYLES_SCSS = """
+/* resets */
+html, body, h1, h2, h3, h4, h5, h6 {
+  margin: 0;
+  padding: 0;
+}
+
+/* SASS stuff */
+@mixin bg-image-2x($path, $ext: "png") {
+  $at1x_path: "#{$path}.#{$ext}";
+  $at2x_path: "#{$path}@2x.#{$ext}";
+  background-image: url("#{$at1x_path}");
+
+  @media all and (-webkit-min-device-pixel-ratio : 1.5),
+         all and (-o-min-device-pixel-ratio: 3/2),
+         all and (min--moz-device-pixel-ratio: 1.5),
+         all and (min-device-pixel-ratio: 1.5) {
+    background-image: url("#{$at2x_path}");
+  }
+}
+
+/* typography */
+html, body {
+  font-kerning: normal;
+  font-feature-settings: "kern";
+  -webkit-font-feature-settings: "kern";
+  font-smoothing: antialiased;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+}
+"""
+
+# TODO:  give this puppy a port lookup. these project absolutely should
+# be stateless.
+SAMPLE_MAKEFILE = """
+s3_bucket_name = www.cedriceats.com
+deploy:
+	s3put -b $(s3_bucket_name) --header "Content-Type=text/html" -p "`pwd`/build" build/*
+	s3put -b $(s3_bucket_name) -p "`pwd`" assets/css/
+deploy-assets:
+	s3put -b $(s3_bucket_name) -p "`pwd`" assets/*
+clean:
+	rm -fr build/
+port = 8809
+run:
+	open -a 'Google Chrome' "http://localhost:$(port)"
+	python -m SimpleHTTPServer $(port)
+"""
+
+SAMPLE_BASE_TEMPLATE = """
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Bootstrap JS -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<title>{% block title %}{% endblock %}</title>
+<link rel="stylesheet" href="/assets/css/styles.css">
+
+<body>
+  {% block content %}
+  {% endblock %}
+</body>
+"""
+
+SAMPLE_HOME_TEMPLATE = """
+{% extends "base.html" %}
+
+{% block title %}Home{% endblock title %}
+{% block content %}
+
+{% endblock content %}
+"""
 
 SAMPLE_ROUTES = """
 {
   "routes": [
     {
-      "path": "index.html", 
+      "path": "index.html",
+      "template": "home.html",
       "data": {
-        "fish": [
-            "sticks"
-        ]
-      }, 
-      "template": "home.html"
+        "fish": "sticks"
+      }
     }
   ]
 }
@@ -23,13 +103,13 @@ config_fn = '.liz.config'
 
 SAMPLE_CONFIG = """
 {
-  "routes": "routes.json", 
-  "templates": "templates", 
+  "routes": "routes.json",
+  "templates": "templates",
   "build": "build/"
 }
 """
 
-IS_VERBOSE = True 
+IS_VERBOSE = True
 
 def _pprint(data):
     import json
@@ -46,12 +126,12 @@ def init(opts, args):
         .liz.config
         /templates
         routes.json
-    
-    This can be used as a starting point for developing a static site. 
+
+    This can be used as a starting point for developing a static site.
     You can create a Makefile to handle deployments once the build is
     finished. Another good idea is to create an assets/ directory to
     handle static files like JS, SCSS and images. The Makefile can
-    deploy each of these separately.  
+    deploy each of these separately.
     """
     if os.path.exists(config_fn):
         _fatal("project already initialized.")
@@ -62,6 +142,37 @@ def init(opts, args):
     default_routes_fn = 'routes.json'
     with open(default_routes_fn, 'w') as f:
         f.write(SAMPLE_ROUTES)
+
+    # Make sure the project has a Makefile.
+    with open('Makefile', 'w') as f:
+        f.write(SAMPLE_MAKEFILE)
+
+    # Start the project off with a SCSS snippet.
+    styles_dir = 'assets/css'
+    try:
+        os.makedirs(styles_dir)
+    except OSError:
+        if not os.path.isdir(styles_dir):
+            raise
+    sample_styles_fn = 'styles.scss'
+    with open(os.path.join(styles_dir, sample_styles_fn), 'w') as f:
+        f.write(SAMPLE_STYLES_SCSS)
+
+    # Write starter templates so you can get running.
+    templates_dir = json.loads(SAMPLE_CONFIG)['templates']
+    try:
+        os.makedirs(templates_dir)
+    except OSError:
+        if not os.path.isdir(templates_dir):
+            raise
+
+    sample_base = 'base.html'
+    with open(os.path.join(templates_dir, sample_base), 'w') as f:
+        f.write(SAMPLE_BASE_TEMPLATE)
+
+    sample_home_template = 'home.html'
+    with open(os.path.join(templates_dir, sample_home_template), 'w') as f:
+        f.write(SAMPLE_HOME_TEMPLATE)
 
 def build(opts=None, args=None):
     """Build a liz project.
@@ -74,7 +185,7 @@ def build(opts=None, args=None):
     import os
     import sys
     from jinja2 import Environment, FileSystemLoader
-    
+
     # Make sure we're in a liz project and it's properly configured.
     if not os.path.exists(config_fn):
         print "fatal: not a liz project."
@@ -89,7 +200,7 @@ def build(opts=None, args=None):
 
     # Parse command arguments.
     try:
-        nopts, nargs = getopt.getopt(args, 
+        nopts, nargs = getopt.getopt(args,
             "vhs:", ["verbose', help", "path-suffix="])
         if nargs:
             _fatal("'build' doesn't accept any arguments.")
@@ -148,7 +259,7 @@ def build(opts=None, args=None):
         return path
     try:
         # By default, the list of routes is an attribute called
-        # "routes". 
+        # "routes".
         routes = routes_d["routes"]
         for route in routes:
             if 'name' in route:
@@ -164,7 +275,7 @@ def build(opts=None, args=None):
     for route in routes:
         template = route.get('template')
         # Some routes are for external routing.
-        if not template:  
+        if not template:
             continue
         loader = env.get_template(template)
         content = loader.render(route.get('data', {}))
@@ -175,7 +286,7 @@ def build(opts=None, args=None):
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, 
+        opts, args = getopt.getopt(argv,
             "vhr:", ["verbose", "help", "routes="])
     except getopt.GetoptError:
         print __doc__
